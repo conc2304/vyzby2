80ce78270c79ba09f5d4ad87565da2517379f13f
clyzby <jose.conchello@gmail.com>
2020-04-17 11:45:15 -0400

    master - initial commit of vue with ts support

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,15 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     HelloWorld
   }
-}
+};
 </script>
+
